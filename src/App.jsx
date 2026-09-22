5a6fbc4b972360b5b9e3745822025cf718b80639
HatimBhaijee <hatim.bhaijee.2006@gmail.com>
2026-09-22 10:59:37 +0300

    updated to server url

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ export default function App() {
   const [paymentStatus, setPaymentStatus] = useState("");
 
 
- const serverurl = "http://localhost:3000/api"
-//  const serverurl = "http://173.249.17.168:81/api"
+//  const serverurl = "http://localhost:3000/api"
+ const serverurl = "http://173.249.17.168:81/api"
 
   useEffect(() => {
     getData();
